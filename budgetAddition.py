d65e106449d492692084562db7ac25a34ba9afd7
Ben Sherwood <bens37@ksu.edu>
2023-12-15 20:25:30 -0600

    Added testing

--- budgetAddition.py
+++ budgetAddition.py
@@ -2,8 +2,3 @@
 def add_to_budget(moneyToAdd, currentBudget):
     newBudget = currentBudget + moneyToAdd
     return newBudget
-    
-currentBudget = 15
-newMoney = 8
-newBudget = add_to_budget(newMoney, currentBudget)
-print(f"You added ${newMoney} to your previous budget of ${currentBudget}. Your new budget is: ${newBudget}")
