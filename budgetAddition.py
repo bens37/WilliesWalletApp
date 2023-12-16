661a947a74ac8320ce045b8a562ea695eb0bde0a
bens37 <73571185+bens37@users.noreply.github.com>
2023-12-15 22:05:44 -0600

    Update budgetAddition.py

--- budgetAddition.py
+++ budgetAddition.py
@@ -1,4 +1,4 @@
-''' Adds money to the current budget '''
+''' Adds money to the current budget this is an edit'''
 def add_to_budget(moneyToAdd, currentBudget):
     newBudget = currentBudget + moneyToAdd
     return newBudget
