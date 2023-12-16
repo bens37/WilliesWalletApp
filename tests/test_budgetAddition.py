0d50cf9d21223a593c2d8d8348c18fa5d480d489
bens37 <73571185+bens37@users.noreply.github.com>
2023-12-15 20:55:29 -0600

    Update test_budgetAddition.py

--- tests/test_budgetAddition.py
+++ tests/test_budgetAddition.py
@@ -1,21 +1,21 @@
 import pytest
-from .. import budgetAddition
+from budgetAddition import add_to_budget
 
 def test_add_to_budget():
     # Test case 1
     current_budget = 15
     money_to_add = 8
-    new_budget = budgetAddition.add_to_budget(money_to_add, current_budget)
+    new_budget = add_to_budget(money_to_add, current_budget)
     assert new_budget == 23
 
     # Test case 2
     current_budget = 0
     money_to_add = 10
-    new_budget = budgetAddition.add_to_budget(money_to_add, current_budget)
+    new_budget = add_to_budget(money_to_add, current_budget)
     assert new_budget == 10
 
     # Test case 3 (negative money)
     current_budget = 30
     money_to_add = -5
-    new_budget = budgetAddition.add_to_budget(money_to_add, current_budget)
+    new_budget = add_to_budget(money_to_add, current_budget)
     assert new_budget == 25
